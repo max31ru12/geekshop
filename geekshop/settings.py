c32154dff55770f0c790e845842491703a507c11
max31ru12 <maxevg72@gmail.com>
2022-03-16 12:24:09 -0700

    Lesson 4. Auth app and user model

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "mainapp",
+    'authnapp',
 ]
 
 MIDDLEWARE = [
@@ -136,3 +137,5 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 MEDIA_URL = "/media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+AUTH_USER_MODEL = 'authnapp.ShopUser'
